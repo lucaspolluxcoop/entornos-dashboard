1423d3a040a86cab9dfd5ccd7a054ca2a109d577
Lucas Pollux <lucas@polluxcoop.com>
2022-07-16 21:06:21 -0300

    completar vista de contrato con extinción

--- components/contracts/ContractData.vue
+++ components/contracts/ContractData.vue
@@ -8,61 +8,65 @@
         </h3>
         <div class="row mb-4">
           <div class="col-sm-12 col-md-2 my-1">
-            <div>
-              <p class="h5 mb-0">Inicio de Contrato</p>
-              <p>{{ contract.startContract | date }}</p>
-            </div>
+            <p class="h5 mb-0">Inicio de Contrato</p>
+            <p>{{ contract.startContract | date }}</p>
           </div>
           <div class="col-sm-12 col-md-2 my-1">
-            <div>
-              <p class="h5 mb-0">Fin de Contrato</p>
-              <p>{{ contract.endContract | date }}</p>
-            </div>
+            <p class="h5 mb-0">Fin de Contrato</p>
+            <p>{{ contract.endContract | date }}</p>
           </div>
           <div class="col-sm-12 col-md-8 my-1">
-            <div>
-              <p class="h5 mb-0">Propiedad</p>
-              <p>{{ getPropertyData }}</p>
-            </div>
+            <p class="h5 mb-0">Propiedad</p>
+            <p>{{ getPropertyData }}</p>
           </div>
           <div class="col-sm-12 col-md-4 my-1">
-            <div>
-              <p class="h5 mb-0">Canon Locativo</p>
-              <p v-if="contract.contractLocativeCanon">
-                {{ contract.contractLocativeCanon.title }}
-              </p>
-              <p v-else>-</p>
-            </div>
+            <p class="h5 mb-0">Canon Locativo</p>
+            <p v-if="contract.contractLocativeCanon">
+              {{ contract.contractLocativeCanon.title }}
+            </p>
+            <p v-else>-</p>
           </div>
           <div class="col-sm-12 col-md-4 my-1">
-            <div>
-              <p class="h5 mb-0">Tipo del Contrato</p>
-              <p>{{ contract.contractType.title }}</p>
-            </div>
+            <p class="h5 mb-0">Tipo del Contrato</p>
+            <p>{{ contract.contractType.title }}</p>
           </div>
           <div class="col-sm-12 col-md-4 my-1">
-            <div>
-              <p class="h5 mb-0">Locatario</p>
-              <p>
-                {{ contract.tenant.profile.cuit || '' }}
-                {{
-                  contract.tenant.profile.firstName +
-                  contract.tenant.profile.lastName
-                }}
-              </p>
-            </div>
+            <p class="h5 mb-0">Locatario</p>
+            <p>
+              {{ contract.tenant.profile.cuit || '' }}
+              {{
+                contract.tenant.profile.firstName +
+                contract.tenant.profile.lastName
+              }}
+            </p>
           </div>
-          <div class="col-sm-12 col-md-4 my-1">
-            <div>
-              <p class="h5 mb-0">Locador</p>
-              <p>
-                {{ contract.locator.profile.cuit || '' }}
-                {{
-                  contract.locator.profile.firstName +
-                  contract.locator.profile.lastName
-                }}
-              </p>
-            </div>
+          <div class="col-sm-12 col-md-12 my-1">
+            <p class="h5 mb-0">Locador</p>
+            <p>
+              {{ contract.locator.profile.cuit || '' }}
+              {{
+                contract.locator.profile.firstName +
+                contract.locator.profile.lastName
+              }}
+            </p>
+          </div>
+          <div v-if="contract.extintionDate" class="col-sm-12 col-md-4 my-1">
+            <p class="h5 mb-0">Fecha de extinción</p>
+            <p>
+              {{ contract.extintionDate | date }}
+            </p>
+          </div>
+          <div v-if="contract.extintionDate" class="col-sm-12 col-md-4 my-1">
+            <p class="h5 mb-0">Motivo Extinción</p>
+            <p>
+              {{ contract.extintionReason.title}}
+            </p>
+          </div>
+          <div v-if="contract.extintionReason.id === 9" class="col-sm-12 col-md-4 my-1">
+            <p class="h5 mb-0">Otro motivo</p>
+            <p>
+              {{ contract.otherReason}}
+            </p>
           </div>
         </div>
       </collapse-item>
